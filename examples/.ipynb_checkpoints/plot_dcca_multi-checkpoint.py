20ff3c68c30b41a8401357fd78805053c4d7665c
Dutta <pdutta@login01.cm.cluster>
2022-04-13 21:07:11 -0400

    New message for the combined commit

--- examples/.ipynb_checkpoints/plot_dcca_multi-checkpoint.py
+++ examples/.ipynb_checkpoints/plot_dcca_multi-checkpoint.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(1,'/home/pdutta/DGCCA/cca_zoo')
+sys.path.insert(1,'/home/pdutta/Github/Multiview_clustering_DGCCCA')
 
 """
 Deep CCA for more than 2 views
@@ -7,14 +7,12 @@ Deep CCA for more than 2 views
 
 This example demonstrates how to easily train Deep CCA models and variants
 """
-
+import argparse, json
 import numpy as np
 import pandas as pd
-import cca_zoo.packages.pytorch_lightning.pytorch_lightning as pl
-from torch.utils.data import Subset
+import pytorch_lightning as pl
 from torch import optim
 
-# %%
 from cca_zoo import data
 from cca_zoo.deepmodels import (
     DCCA,
@@ -26,91 +24,66 @@ from cca_zoo.deepmodels import (
     DTCCA,
 )
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', default=None, required=True, help='Tab-separated view data')
+    parser.add_argument('--n', default=None, type=int, required=True, help='Number of views')
+    parser.add_argument('--arch', default=None, help='Architecture for each view network, given as list of lists of layer widths')
+    parser.add_argument('--epochs', default=200, type=int, help='Number of epochs to run for')
+    parser.add_argument('--latDim', default=100, type=int, help='Size of the latent dimensions for each view')
+    parser.add_argument('--lr', default=0.0001, type=float, help='Learning rate')
+    parser.add_argument('--log_path', default=None, required=True, help='path to log files')
+    parser.add_argument('--embedPath', default=None, required=True, help='path to save embedding vector')
+    parser.add_argument('--num_workers', default=8, type=int, required=True, help='number of process for dataloading')
+    parser.add_argument('--train_batch_size', default=16, type=int, required=True, help='training batch size')
+    parser.add_argument('--val_batch_size', default=16, type=int, required=True, help='validation batch size')
+
+    
+    args = parser.parse_args()
+
+    df = pd.read_csv(args.input, sep = "\t", header =None)
+    no_of_views = args.n
+    architecture_list= json.loads(args.arch)
+    num_of_epochs= args.epochs
+    latent_dims= args.latDim
+    lr= args.lr
+    
+
+    ## Preprocess the dataframe into list of lists 
+    feature_lists = []
+    for i in range(no_of_views):
+        feature_lists.append([])
+    for i in range(0,df.shape[0]):
+        #print (len(df.iloc[i, 4]), len(df.iloc[i, 5]), len(df.iloc[i, 6]))
+        #print (len(df.iloc[i, 4].split()), len(df.iloc[i, 5].split()), len(df.iloc[i, 6].split()))
+        feature_lists[0].append(df.iloc[i, 4].split()), feature_lists[1].append(df.iloc[i, 5].split()), feature_lists[2].append(df.iloc[i, 6].split())
+    for k in range(no_of_views):
+        globals()['view_%s_features' % k] = np.array([[float(i) for i in row] for row in feature_lists[k]])
+    print(view_0_features.shape, view_1_features.shape, view_2_features.shape)
+
+    ## Creating training dataset
+    dataset = data.CCA_Dataset([view_0_features, view_1_features, view_2_features])
+    train_dataset, val_dataset = process_data(dataset, val_split=0.2)
+    loader = get_dataloaders(dataset)
+    train_loader, val_loader = get_dataloaders(train_dataset, val_dataset, batch_size=args.train_batch_size, val_batch_size=args.val_batch_size, num_workers= (args.num_workers-10))
 
 
-df = pd.read_csv("/home/pdutta/DGCCA/data/TCGA_BRCA/methyl_rnaseq_mirna_minmax.tsv", sep = "\t", header =None)
-
-print (df.shape)
-print (df.iloc[1,0:10])
-
-
-
-
-
-X_feature_list =  []
-Y_feature_list =  []
-Z_feature_list =  []
-
-for i in range(0,1233):
-    #print (len(df.iloc[i, 4]), len(df.iloc[i, 5]), len(df.iloc[i, 6]))
-    #print (len(df.iloc[i, 4].split()), len(df.iloc[i, 5].split()), len(df.iloc[i, 6].split()))
-    X_feature_list.append(df.iloc[i, 4].split()), Y_feature_list.append(df.iloc[i, 5].split()), Z_feature_list.append(df.iloc[i, 6].split())
-
-
-X_features= np.array([[float(i) for i in row] for row in X_feature_list])
-Y_features= np.array([[float(i) for i in row] for row in Y_feature_list])
-Z_features= np.array([[float(i) for i in row] for row in Z_feature_list])
-print(X_features.shape, Y_features.shape, Z_features.shape)
-print (Z_features)
-print (X_features.shape[1])
-input()
-
-dataset = data.CCA_Dataset([X_features, Y_features, Z_features])
-train_dataset, val_dataset = process_data(dataset, val_split=0.2)
-loader = get_dataloaders(dataset)
-train_loader, val_loader = get_dataloaders(train_dataset, val_dataset)
-# for item in next(iter(train_loader)):
-#     print ("#",len(item))
-#     print ( item[0].shape)
-#     input()
-#     print (item)
-#     input()
-
-
-
-#print ("$$$",len(train_loader))
-# for item in next(iter(train_loader)):
-#     print ("#",item,len(item))
-#     print (item[0], item[0].shape)
-
-# The number of latent dimensions across models
-latent_dims = min(X_features.shape[1], Y_features.shape[1], Z_features.shape[1])
-print("latent dims", latent_dims)
-input()
-
-
-# number of epochs for deep models
-epochs = 100
-no_of_views  = 3
+    # Developing the architecture
+    for i in range(no_of_views):
+        globals()['Encoder_%s' % i]=architectures.Encoder(latent_dims=latent_dims, feature_size=globals()["view_%s_features" %i].shape[1], layer_sizes=architecture_list[i])
 
 
 
+    # Deep Generalized Canonical Correlation Analysis
+    dcca = DCCA(
+        latent_dims=latent_dims, encoders=[Encoder_0, Encoder_1, Encoder_2], objective=objectives.GCCA
+    )
+    optimizer = optim.Adam(dcca.parameters(), lr=lr)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, 1)
+    dcca = CCALightning(dcca, optimizer=optimizer, lr_scheduler=scheduler)
+    trainer = pl.Trainer(default_root_dir=args.log_path, max_epochs=num_of_epochs, enable_checkpointing=False, accelerator="gpu", devices=4, strategy="ddp")
+    #, accelerator="gpu", devices=4, strategy="ddp"#### gpus=2
+    trainer.fit(dcca, train_loader, val_loader)
 
 
 
-encoder_1 = architectures.Encoder(latent_dims=latent_dims, feature_size=X_features.shape[1], layer_sizes=[500, 100])
-encoder_2 = architectures.Encoder(latent_dims=latent_dims, feature_size=Y_features.shape[1], layer_sizes=[500, 100])
-encoder_3 = architectures.Encoder(latent_dims=latent_dims, feature_size=Z_features.shape[1], layer_sizes=[500, 100])
-
-
-# Deep GCCA
-dcca = DCCA(
-    latent_dims=latent_dims, encoders=[encoder_1, encoder_2, encoder_3], objective=objectives.GCCA
-)
-print ("Return DCCA")
-
-
-optimizer = optim.Adam(dcca.parameters(), lr=1e-3)
-scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, 1)
-dcca = CCALightning(dcca, optimizer=optimizer, lr_scheduler=scheduler)
-
-print ("CCALightning dcca", dcca)
-        
-
-trainer = pl.Trainer(max_epochs=epochs, enable_checkpointing=False)
-print ("Check")
-trainer.fit(dcca, train_loader, val_loader)
-print ("ded")
-
-
- 
--- examples/scripts/.ipynb_checkpoints/dgcca_embedding-checkpoint.sh
+++ examples/scripts/.ipynb_checkpoints/dgcca_embedding-checkpoint.sh
@@ -1,62 +1,38 @@
+##Directory Path
 IN_DIR="/home/pdutta/DGCCA/data/TCGA_BRCA"
-# OUT_DIR="/projects/b1017/Jerry/cancer_subtyping/results/LUAD/DGCCA"
+OUT_DIR="/home/pdutta/DGCCA/results"
 
-# # Raw data
-IN_PATH="${IN_DIR}/methyl_rnaseq_mirna_minmax1.tsv"
+## Raw data
+IN_PATH="${IN_DIR}/methyl_rnaseq_mirna_minmax.tsv"
 NO_OF_VIEWS=3
-# TRAIN_PATH="${IN_DIR}/methyl_rnaseq_mirna_minmax1.train.pkl"
-# TUNE_PATH="${IN_DIR}/methyl_rnaseq_mirna_minmax1.tune.pkl"
-# EXP=1
 
-# # Some reasonable defaults
-# #lr=0.005 # Too big a step size for many of the models
-# lr=0.000001
-# activation="relu"
-# bsize=617 # minibatch size
-# numEpochs=20
-# truncParam=100 # How many left singular vectors to keep in our data matrices
-# valfreq=1 # Number of epochs between checking reconstruction error
 
-# # Optimizers
-# # adam_opt="{\"type\":\"adam\",\"params\":{\"adam_b1\":0.1,\"adam_b2\":0.001,\"learningRate\":${lr}}}"
-# # sgd_momentum_opt="{\"type\":\"sgd_momentum\",\"params\":{\"momentum\":0.99,\"decay\":1.0,\"learningRate\":${lr}}}"
-# sgd_opt="{\"type\":\"sgd\",\"params\":{\"decay\":1.0,\"learningRate\":${lr}}}"
+## Training parameters
+architecture="[[1000,500],[900,400],[200,150]]" # Set architecture of each view
+numEpochs=100
+latent_dims=100
+lr=0.000001
+train_batch_size=32
+val_batch_size=32
+num_workers=$(nproc --all)
 
-# # Training parameters
-# architecture="[[19924,500,100],[16630,500,100],[220,500,100]]" # Set architecture of each view
-# k=100
-# # rcovStr="0.000001"
-# # l1=0.0001
-# # l2=0.01
-# l1=0.001
-# l2=0.0001
-# vnameStr="methylation rnaseq mirna"
-# # vweights="1.0" # How much to weight each view
 
-# # drop spaces
-# archStr=${architecture// /}
-# # vweightStr=${vweights// /,}
-# # rcovStrCat=${rcovStr// /,}
+## Where to write output
+archStr=${architecture// /}
+BASE="embedding_views=${NO_OF_VIEWS}_numEpochs=${numEpochs}_val_batch_size=${val_batch_size}__train_batch_size=${train_batch_size}_arch=${archStr}"
 
-# # some modifications
-# vweightStr="1.0 1.0 1.0"
-# rcov=0.000001
-
-# # Where to write output
-# BASE="embedding_dgcca_wts=${vweights}_k=${k}_rcov=${rcovStrCat}_arch=${archStr}_l1=${l1}_l2=${l2}_exp=${EXP}"
-
-# mkdir "${OUT_DIR}/embeddings"
+mkdir -p "${OUT_DIR}/embeddings"
 # mkdir "${OUT_DIR}/models"
 # mkdir "${OUT_DIR}/logs"
 
-# EMBEDDINGS_PATH="${OUT_DIR}/embeddings/${BASE}.embedding.npz"
+EMBEDDINGS_PATH="${OUT_DIR}/embeddings/"
 # MODEL_PATH="${OUT_DIR}/models/${BASE}.model.npz"
-# LOG_PATH="${OUT_DIR}/logs/${BASE}.log.txt"
+LOG_PATH="${OUT_DIR}/logs"
 # HISTORY_PATH="${OUT_DIR}/logs/${BASE}.history.npz"
 
-# echo "python ../dgcca_train_harness.py --input ${IN_PATH} --preptrain ${TRAIN_PATH} --preptune ${TUNE_PATH} --output ${EMBEDDINGS_PATH} --model ${MODEL_PATH} --arch \"${architecture}\" --truncparam ${truncParam} --k ${k} --rcov ${rcovStrCat} --batchSize ${bsize} --l1 ${l1} --l2 ${l2} --vnames ${vnameStr} --activation ${activation} --weights ${vweightStr} --opt \"${sgd_opt}\" --epochs ${numEpochs} --valfreq ${valfreq} --lcurvelog ${HISTORY_PATH} | tee ${LOG_PATH}"
-# python ../dgcca_train_harness.py --input ${IN_PATH} --preptrain ${TRAIN_PATH} --preptune ${TUNE_PATH} --output ${EMBEDDINGS_PATH} --model ${MODEL_PATH} --arch "${architecture}" --truncparam ${truncParam} --k ${k} --rcov ${rcov} ${rcov} ${rcov} --batchSize ${bsize} --l1 ${l1} --l2 ${l2} --vnames ${vnameStr} --activation ${activation} --weights ${vweightStr} --opt "${sgd_opt}" --epochs ${numEpochs} --valfreq ${valfreq} --lcurvelog ${HISTORY_PATH} | tee ${LOG_PATH}
+#echo "python ../dgcca_train_harness.py --input ${IN_PATH} --preptrain ${TRAIN_PATH} --preptune ${TUNE_PATH} --output ${EMBEDDINGS_PATH} --model ${MODEL_PATH} --arch \"${architecture}\" --truncparam ${truncParam} --k ${k} --rcov ${rcovStrCat} --batchSize ${bsize} --l1 ${l1} --l2 ${l2} --vnames ${vnameStr} --activation ${activation} --weights ${vweightStr} --opt \"${sgd_opt}\" --epochs ${numEpochs} --valfreq ${valfreq} --lcurvelog ${HISTORY_PATH} | tee ${LOG_PATH}"
+#python ../dgcca_train_harness.py --input ${IN_PATH} --preptrain ${TRAIN_PATH} --preptune ${TUNE_PATH} --output ${EMBEDDINGS_PATH} --model ${MODEL_PATH} --arch "${architecture}" --truncparam ${truncParam} --k ${k} --rcov ${rcov} ${rcov} ${rcov} --batchSize ${bsize} --l1 ${l1} --l2 ${l2} --vnames ${vnameStr} --activation ${activation} --weights ${vweightStr} --opt "${sgd_opt}" --epochs ${numEpochs} --valfreq ${valfreq} --lcurvelog ${HISTORY_PATH} | tee ${LOG_PATH}
 
-echo "python ../dgcca_train_harness.py --input ${IN_PATH} --n ${NO_OF_VIEWS}
+echo "python ../plot_dcca_multi.py --input ${IN_PATH} --n ${NO_OF_VIEWS} --arch \"${architecture}\" --epochs ${numEpochs} --latDim ${latent_dims} --lr ${lr} --log_path ${LOG_PATH} --embedPath ${EMBEDDINGS_PATH} --num_workers ${num_workers} --train_batch_size ${train_batch_size} --val_batch_size ${val_batch_size}"
 
-python ../dgcca_train_harness.py --input ${IN_PATH} --n ${NO_OF_VIEWS}
+python ../plot_dcca_multi.py --input ${IN_PATH} --n ${NO_OF_VIEWS} --arch "${architecture}" --epochs ${numEpochs} --latDim ${latent_dims} --lr ${lr} --log_path ${LOG_PATH} --embedPath ${EMBEDDINGS_PATH} --num_workers ${num_workers} --train_batch_size ${train_batch_size} --val_batch_size ${val_batch_size}
