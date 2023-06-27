7324879caf070ce0f471bfc88962ee49c7ec1726
mattobu83 <57159222+mattobu83@users.noreply.github.com>
2023-06-27 15:35:42 -0400

    Update dgcca_embedding_LUAD_epoch.sh

--- examples/scripts/dgcca_embedding_LUAD_epoch.sh
+++ examples/scripts/dgcca_embedding_LUAD_epoch.sh
@@ -16,7 +16,7 @@ do
   
   ## Training parameters
   architecture="[[1000,500],[900,400],[200,150]]" # Set architecture of each view
-  numEpochs= $epoch
+  #numEpochs= $epoch
   latent_dims=100
   lr=0.0000001
   train_batch_size=32
@@ -26,7 +26,7 @@ do
   
   ## Where to write output
   archStr=${architecture// /}
-  BASE="views=${NO_OF_VIEWS}_numEpochs=${numEpochs}__val_batch_size=${val_batch_size}__train_batch_size=${train_batch_size}__arch=${archStr}__lr=${lr}__latDim=${latent_dims}"
+  BASE="views=${NO_OF_VIEWS}_numEpochs=${epoch}__val_batch_size=${val_batch_size}__train_batch_size=${train_batch_size}__arch=${archStr}__lr=${lr}__latDim=${latent_dims}"
   EMBEDDINGS_PATH="${OUT_DIR}/embeddings/${BASE}"
   #rm -r "${EMBEDDINGS_PATH}"
   mkdir -p "${EMBEDDINGS_PATH}"
@@ -38,8 +38,8 @@ do
   
   ## Run the code
   
-  echo "python /content/DeepMOIS-MC/examples/dcca_multi.py --input ${IN_PATH} --n ${NO_OF_VIEWS} --arch \"${architecture}\" --epochs ${numEpochs} --latDim ${latent_dims} --lr ${lr} --log_path ${LOG_PATH} --model_path ${MODEL_PATH} --embedPath ${EMBEDDINGS_PATH} --num_workers ${num_workers} --train_batch_size ${train_batch_size} --val_batch_size ${val_batch_size} --base_name ${BASE} --cancer_type ${CANCER_TYPE} --final_embed_path ${FINAL_EMBEDDING_PATH}"
+  echo "python /content/DeepMOIS-MC/examples/dcca_multi.py --input ${IN_PATH} --n ${NO_OF_VIEWS} --arch \"${architecture}\" --epochs ${epoch} --latDim ${latent_dims} --lr ${lr} --log_path ${LOG_PATH} --model_path ${MODEL_PATH} --embedPath ${EMBEDDINGS_PATH} --num_workers ${num_workers} --train_batch_size ${train_batch_size} --val_batch_size ${val_batch_size} --base_name ${BASE} --cancer_type ${CANCER_TYPE} --final_embed_path ${FINAL_EMBEDDING_PATH}"
   
   # --train_batch_size ${train_batch_size} --val_batch_size ${val_batch_size}
-  python /content/DeepMOIS-MC/examples/dcca_multi.py --input ${IN_PATH} --n ${NO_OF_VIEWS} --arch "${architecture}" --epochs ${numEpochs} --latDim ${latent_dims} --lr ${lr} --log_path ${LOG_PATH} --model_path ${MODEL_PATH} --embedPath ${EMBEDDINGS_PATH} --num_workers ${num_workers} --base_name ${BASE} --cancer_type ${CANCER_TYPE} --final_embed_path ${FINAL_EMBEDDING_PATH} | tee $LOG_PATH"/"$(date "+%Y-%m-%d_%H:%M:%S")"_log.txt"
+  python /content/DeepMOIS-MC/examples/dcca_multi.py --input ${IN_PATH} --n ${NO_OF_VIEWS} --arch "${architecture}" --epochs ${epoch} --latDim ${latent_dims} --lr ${lr} --log_path ${LOG_PATH} --model_path ${MODEL_PATH} --embedPath ${EMBEDDINGS_PATH} --num_workers ${num_workers} --base_name ${BASE} --cancer_type ${CANCER_TYPE} --final_embed_path ${FINAL_EMBEDDING_PATH} | tee $LOG_PATH"/"$(date "+%Y-%m-%d_%H:%M:%S")"_log.txt"
 done
