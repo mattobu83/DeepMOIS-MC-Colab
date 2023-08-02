71fe714779bbad47a325c00a1a79f21d32ea2d93
mattobu83 <57159222+mattobu83@users.noreply.github.com>
2023-08-02 01:54:17 -0400

    Update dgcca_embedding_LUAD_4view_dup.sh

--- examples/scripts/dgcca_embedding_LUAD_4view_dup.sh
+++ examples/scripts/dgcca_embedding_LUAD_4view_dup.sh
@@ -5,12 +5,12 @@ OUT_DIR="./results/${CANCER_TYPE}"
 
 
 ## Raw data
-IN_PATH="${IN_DIR}/methyl_rnaseq_mirna_img_minmax_avg.tsv"
+IN_PATH="${IN_DIR}/methyl_rnaseq_mirna_img_minmax_dup.tsv"
 NO_OF_VIEWS=4
 
 
 ## Training parameters
-architecture="[[1000,500],[900,400],[200,150]]" # Set architecture of each view
+architecture="[[1000,500],[900,400],[200,150],[200,150]]" # Set architecture of each view
 numEpochs=5
 latent_dims=100
 lr=0.0000001
