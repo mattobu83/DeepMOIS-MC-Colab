c70f0275ab2dbd6ab7f9a30629323b754962586d
mattobu83 <57159222+mattobu83@users.noreply.github.com>
2023-08-14 11:49:06 -0400

    Update dcca_multi4.py

--- examples/dcca_multi4.py
+++ examples/dcca_multi4.py
@@ -178,7 +178,8 @@ if __name__ == '__main__':
                 #dict_parameters['csv_path']=os.path.join(final_embd_csv_path, "final_embedding.csv")
                 np.save(os.path.join(final_embd_csv_path, 'parameters_details.npy'), dict_parameters)
                 #save_embedding(args.embedPath, args.base_name, args.final_embed_path, shuffled_indices, dict_parameters)
-            except:
+            except Exception as e:
+                print(e):
                 print("-x-"*25)
                 print(" ")
                 print("did not converge")
